735af6316f0b5b10ae5c0b8dc90b85d9d2d866a0
Ryan Taylor <ryan.taylor1@yahoo.com>
2022-12-31 21:52:18 -0800

    added to product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,22 +1,70 @@
 const router = require('express').Router();
+const sequelize = require('sequelize');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category}, {model: Tag}],
+      attributes: {
+        include: [
+          [
+            sequelize.literal()
+          ],
+        ],
+      },
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try{
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category}, { model: Tag }],
+      attributes: {
+        include: [
+          [
+            sequelize.literal()
+          ],
+        ],
+      },
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with that id!'});
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      tagIds: req.body.tagIds,
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
